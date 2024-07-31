8fb73a3460d6c2b688bdebd48d0062432f34cd97
Brean Julius Carbonilla <julius.carbonilla@gmail.com>
2024-07-31 20:46:06 +1000

    Initial Commit

---
+++ React-App/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Accordian from "./components/Accordian";
+
+function App() {
+    return (
+        <>
+            <div className="App">
+                accordian
+            </div>
+        </>
+    );
+}
+
+export default App;
