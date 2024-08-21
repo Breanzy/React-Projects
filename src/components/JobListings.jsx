f610ff17352c38fa20c5e9d06e0f5947429e448c
Brean Julius Carbonilla <julius.carbonilla@gmail.com>
2024-08-22 08:11:29 +1000

    Using Data Loader insteaad of UseState for fetching data from json-server

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -8,9 +8,7 @@ const JobListings = ({ isHome = false }) => {
 
     useEffect(() => {
         const fetchJobs = async () => {
-            const apiUrl = isHome
-                ? "/api/jobs?_limit=3"
-                : "/api/jobs";
+            const apiUrl = isHome ? "/api/jobs?_limit=3" : "/api/jobs";
             try {
                 const res = await fetch(apiUrl);
                 const data = await res.json();
